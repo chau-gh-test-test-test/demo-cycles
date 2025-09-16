6ba1945213d314650c0080ac555421dce2e4cc3c
Chau <25516557+nartc@users.noreply.github.com>
2025-09-15 23:23:35 -0500

    new cycle

--- libs/feature-admin/src/lib/feature-admin.tsx
+++ libs/feature-admin/src/lib/feature-admin.tsx
@@ -1,11 +1,13 @@
 import styles from './feature-admin.module.css';
 import { DemoCyclesUiLayout } from '@demo-cycles/ui-layout';
+import { DemoCyclesApiUsers } from '@demo-cycles/api-users';
 
 export function DemoCyclesFeatureAdmin() {
   return (
     <div className={styles['container']}>
       <h1>Welcome to DemoCyclesFeatureAdmin!</h1>
       <DemoCyclesUiLayout />
+      <DemoCyclesApiUsers />
     </div>
   );
 }
