61637ea2d377a17012a7cab5788832ac9250dc25
Chau <25516557+nartc@users.noreply.github.com>
2025-09-17 07:59:15 -0500

    break api-users -> ui-theme

--- libs/api-users/src/lib/api-users.tsx
+++ libs/api-users/src/lib/api-users.tsx
@@ -1,13 +1,11 @@
 import styles from './api-users.module.css';
 import { DemoCyclesFeatureChat } from '@demo-cycles/feature-chat';
-import { DemoCyclesUiTheme } from '@demo-cycles/ui-theme';
 
 export function DemoCyclesApiUsers() {
   return (
     <div className={styles['container']}>
       <h1>Welcome to DemoCyclesApiUsers!</h1>
       <DemoCyclesFeatureChat />
-      <DemoCyclesUiTheme />
     </div>
   );
 }
